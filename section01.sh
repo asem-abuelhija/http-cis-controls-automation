7d439ae2a26875a7becc5d708619695faf8df6e2
Asem Abuelhija <asem.adnan@gmail.com>
2022-02-06 15:43:40 +0200

    finish section 1

--- section01.sh
+++ section01.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+CYAN='\033[1;36m'
+NC='\033[0m'
+
+echo -e "1.1: Ensure the Pre-Installation Planning Checklist Has Been Implemented ===> ${CYAN}Not Scored${NC}"
+echo -e "1.2: Ensure the Server Is Not a Multi-Use System ===> ${CYAN}Not Scored${NC}"
+echo -e "1.3: Ensure Apache Is Installed From the Appropriate Binaries ===> ${CYAN}Not Scored${NC}"
+
