27898ed8d3ec0944aa298e625719969c8d8d22c3
Asem Abuelhija <asem.adnan@gmail.com>
2022-02-06 14:59:40 +0200

    add generic conditions to section 10

--- section10.sh
+++ section10.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+#LimitRequestLine=$(grep 'LimitRequestLine' $APACHE_PREFIX/conf/httpd.conf 2>/dev/null)
+#echo $LimitRequestLine
+#10.1
+if [[ ! $(grep 'LimitRequestLine' $APACHE_PREFIX/conf/httpd.conf) ]]; then ## 512 or less
+./check.sh "10.1: Ensure the LimitRequestLine directive is Set to 512 or less" FAIL;
+fi
+
+#10.1
+if [[ ! $(grep 'LimitRequestFields' $APACHE_PREFIX/conf/httpd.conf) ]]; then ## 512 or less
+./check.sh "10.2: Ensure the LimitRequestFields Directive is Set to 100 or Less" FAIL;
+fi
+
+#10.1
+if [[ ! $(grep 'LimitRequestFieldsize' $APACHE_PREFIX/conf/httpd.conf) ]]; then ## 512 or less
+./check.sh "10.3: Ensure the LimitRequestFieldsize Directive is Set to 1024 or Less" FAIL;
+fi
+
+#10.1
+if [[ ! $(grep 'LimitRequestBody' $APACHE_PREFIX/conf/httpd.conf) ]]; then ## 512 or less
+./check.sh "10.4: Ensure the LimitRequestBody Directive is Set to 102400 or Less" FAIL;
+fi
+
