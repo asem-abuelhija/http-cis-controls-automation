08e741a06b13f95912179cc387d713bc7dc0cffb
Asem Abuelhija <asem.adnan@gmail.com>
2022-02-06 15:44:45 +0200

    change identation sections 11 & 12

--- section11.sh
+++ section11.sh
@@ -2,5 +2,5 @@
 CYAN='\033[1;36m'
 NC='\033[0m'
 if [[ ! $(grep -i 'red hat' /etc/os-release 2>/dev/null) ]]; then
-	echo -e "11: Enable SELinux to Restrict Apache Processes ===> ${CYAN}Not Applicable ${NC} (Not Red Hat)"
+	echo -e "11.0: Enable SELinux to Restrict Apache Processes ===> ${CYAN}Not Applicable ${NC} (Not Red Hat)"
 fi
