b4af25a095ae4a2c0a74fb2de5774ee3830c5c09
Asem Abuelhija <asem.adnan@gmail.com>
2022-02-06 15:30:59 +0200

    create main script

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+./section01.sh
+./section02.sh
+./section03.sh
+./section04.sh
+./section05.sh
+./section06.sh
+./section07.sh
+./section08.sh
+./section09.sh
+./section10.sh
+./section11.sh
+./section12.sh
