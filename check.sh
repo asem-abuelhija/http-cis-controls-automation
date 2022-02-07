99d9094afd4633328cba9e6bc6f106705cbe3cec
Asem Abuelhija <asem.adnan@gmail.com>
2022-02-07 15:56:33 +0200

    enhance output style in check.sh

--- check.sh
+++ check.sh
@@ -5,15 +5,23 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 BIPurple='\033[1;95m'
 
+if [ ${#1} -gt 60 ]; then
+	STR62="${1:0:60}.."
+else
+	STR62="${1}                                                                                  "
+	STR62="${STR62:0:62}"
+fi
+
+
 if [[ $2 == 'PASS' ]] ; then
-echo -e "$1 ===> ${GREEN}PASS${NC}"
+echo -e "$STR62 ===> ${GREEN}PASS${NC}"
 fi
 
 if [[ $2 == 'FAIL' ]] ; then
-echo -e "$1 ===> ${RED}FAIL${NC}"
+echo -e "$STR62 ===> ${RED}FAIL${NC}"
 fi
 
 if [[ $2 == 'MANUAL' ]] ; then
-echo -e "$1 ===> ${BIPurple}Manual Check${NC}"
+echo -e "$STR62 ===> ${BIPurple}Manual Check${NC}"
 fi
 
