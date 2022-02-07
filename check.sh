44d33d2c866d2520b98f044efd28f5bb5fa01283
Asem Abuelhija <asem.adnan@gmail.com>
2022-02-07 15:24:53 +0200

    add manual check output to check.sh

--- check.sh
+++ check.sh
@@ -3,7 +3,7 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
-
+BIPurple='\033[1;95m'
 
 if [[ $2 == 'PASS' ]] ; then
 echo -e "$1 ===> ${GREEN}PASS${NC}"
@@ -12,3 +12,8 @@ fi
 if [[ $2 == 'FAIL' ]] ; then
 echo -e "$1 ===> ${RED}FAIL${NC}"
 fi
+
+if [[ $2 == 'MANUAL' ]] ; then
+echo -e "$1 ===> ${BIPurple}Manual Check${NC}"
+fi
+
