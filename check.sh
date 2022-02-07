5c7626d9d8da3f9b4e9ee8bfe65dfc1920bf3817
Asem Abuelhija <asem.adnan@gmail.com>
2022-02-07 16:06:54 +0200

    enhance output in section 1; add not scored to check.sh

--- check.sh
+++ check.sh
@@ -4,6 +4,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 BIPurple='\033[1;95m'
+CYAN='\033[1;36m'
 
 if [ ${#1} -gt 60 ]; then
 	STR62="${1:0:60}.."
@@ -23,5 +24,9 @@ fi
 
 if [[ $2 == 'MANUAL' ]] ; then
 echo -e "$STR62 ===> ${BIPurple}Manual Check${NC}"
+fi
+
+if [[ $2 == 'NOTSCORED' ]] ; then
+echo -e "$STR62 ===> ${CYAN}Not Scored${NC}"
 fi
 
--- section01.sh
+++ section01.sh
@@ -2,7 +2,7 @@
 CYAN='\033[1;36m'
 NC='\033[0m'
 
-echo -e "1.1: Ensure the Pre-Installation Planning Checklist Has Been Implemented ===> ${CYAN}Not Scored${NC}"
-echo -e "1.2: Ensure the Server Is Not a Multi-Use System ===> ${CYAN}Not Scored${NC}"
-echo -e "1.3: Ensure Apache Is Installed From the Appropriate Binaries ===> ${CYAN}Not Scored${NC}"
+./check.sh "1.1: Ensure the Pre-Installation Planning Checklist Has Been Implemented" NOTSCORED
+./check.sh "1.2: Ensure the Server Is Not a Multi-Use System" NOTSCORED
+./check.sh "1.3: Ensure Apache Is Installed From the Appropriate Binaries" NOTSCORED
 
