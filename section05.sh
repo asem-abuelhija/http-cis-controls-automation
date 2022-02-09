7784b51cf6289056a3eea34ce4ef8c69931933b1
Asem Abuelhija <asem.adnan@gmail.com>
2022-02-09 10:58:48 +0200

    start section 5

--- section05.sh
+++ section05.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+#5.1
+if [[ $(perl -ne 'print if /^ *<Directory *\//i .. /<\/Directory/i' $APACHE_PREFIX/conf/httpd.conf |grep 'Options None') ]]; then
+./check.sh "5.1: Ensure Options for the OS Root Directory Are Restricted" PASS
+else
+./check.sh "5.1: Ensure Options for the OS Root Directory Are Restricted" FAIL
+fi
+
+#5.2
+if [[ $(perl -ne 'print if /^ *<Directory *\//i .. /<\/Directory/i' $APACHE_PREFIX/conf/httpd.conf |grep 'Options None') ]]; then
+./check.sh "5.2: Ensure Options for the Web Root Directory Are Restricted" PASS
+else
+./check.sh "5.2: Ensure Options for the Web Root Directory Are Restricted" FAIL
+fi
+
+#5.3
+if [[ $(perl -ne 'print if /^ *<Directory *\//i .. /<\/Directory/i' $APACHE_PREFIX/conf/httpd.conf |grep 'Options None') ]]; then
+./check.sh "5.3: Ensure Options for Other Directories Are Minimized" PASS
+else
+./check.sh "5.3: Ensure Options for Other Directories Are Minimized" FAIL
+fi
+
+
