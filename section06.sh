3c6fd3934fb6f444c646393985cfa67fc63b71e0
Asem Abuelhija <asem.adnan@gmail.com>
2022-02-13 17:09:42 +0200

    finish section 6

--- section06.sh
+++ section06.sh
@@ -13,3 +13,34 @@ if [[ $(grep 'ErrorLog "syslog:' $APACHE_PREFIX/conf/httpd.conf)  ]]; then
 else
 ./check.sh "6.2: Ensure a Syslog Facility Is Configured for Error Logging" FAIL
 fi
+
+#6.3
+if [[ $(grep '^CustomLog' $APACHE_PREFIX/conf/httpd.conf) || $(grep '^LogFormat' $APACHE_PREFIX/conf/httpd.conf) ]]; then
+./check.sh "6.3: Ensure the Server Access Log Is Configured Correctly" PASS
+else
+./check.sh "6.3: Ensure the Server Access Log Is Configured Correctly" FAIL
+fi
+
+#6.4
+./check.sh "6.4: Ensure Log Storage and Rotation Is Configured Correctly" MANUAL
+
+#6.5
+./check.sh "6.5: Ensure Applicable Patches Are Applied" MANUAL
+
+#6.6
+if [[ $(grep 'security2_module' $APACHE_PREFIX/conf/httpd.conf) ]]; then
+./check.sh "6.6: Ensure ModSecurity Is Installed and Enabled" PASS
+else
+./check.sh "6.6: Ensure ModSecurity Is Installed and Enabled" FAIL
+fi
+
+#6.7
+OWASP=$(find $APACHE_PREFIX/modsecurity.d/activated_rules/ -name 'modsecurity_crs_*.conf' 2>/dev/null| xargs grep '^SecRule ' | wc -l)
+if [[ $OWASP -ge 200 ]]; then
+./check.sh "6.7: Ensure the OWASP ModSecurity Core Rule Set Is Installed and Enabled" PASS
+elif [[ $OS != 'Linux' ]]; then
+./check.sh "6.7: Ensure the OWASP ModSecurity Core Rule Set Is Installed and Enabled" NOTAPPLICABLE
+else
+./check.sh "6.7: Ensure the OWASP ModSecurity Core Rule Set Is Installed and Enabled" FAIL
+fi
+
